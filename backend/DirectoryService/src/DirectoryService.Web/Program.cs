5f92e085ce06cc018f3fa352bba0d55ce7b436d7
GeorgeD615 <g.davlyatshin@gmail.com>
2026-07-12 18:03:44 +0300

    db_config

--- backend/DirectoryService/src/DirectoryService.Web/Program.cs
+++ backend/DirectoryService/src/DirectoryService.Web/Program.cs
@@ -1,3 +1,5 @@
+using DirectoryService.Infrastructure.Postgres;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddHealthChecks();
+
+string connectionString = builder.Configuration.GetConnectionString("DirectoryServiceDb")
+    ?? throw new InvalidOperationException("Connection string 'DirectoryServiceDb' is not configured.");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 WebApplication app = builder.Build();
 
